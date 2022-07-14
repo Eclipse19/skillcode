600ea89191efaf58a76d58cb351f4abf6b88ffd8
sagar sarkar <sagarsarkar2174@gmail.com>
2022-07-15 00:36:55 +0530

    sagar/multiply

---
+++ src/multiply.java
@@ -0,0 +1,7 @@
+public class multiply {
+    public static void main(String[] args) {
+        int a=5,b=10,c;
+        c=a*b;
+        System.out.println("the value of multification :" +c);
+    }
+}
