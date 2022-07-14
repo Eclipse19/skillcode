ce9177ec99ddd8fca996b4cb4689fac859776669
sagar sarkar <sagarsarkar2174@gmail.com>
2022-07-14 23:51:02 +0530

    this is the 3rd commit

--- src/sum.java
+++ src/sum.java
@@ -2,6 +2,6 @@ public class sum{
     public static void main(String[] args) {
          int a = 10 ,b =10;
          int c = a+b;
-         System.out.println(c);
+         System.out.println("this the sum of two number" +c);
     }
 }
